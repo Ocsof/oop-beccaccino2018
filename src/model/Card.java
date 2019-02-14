699ae6d480b80d164e35b9752b6af00c24587309
Davide Alpi <davide.alpi@studio.unibo.it>
2019-02-14 17:07:23 +0100

    First interface create

---
+++ src/model/Card.java
@@ -0,0 +1,22 @@
+package model;
+
+public interface Card {
+	
+	enum Suit{
+		BASTONI, SPADE, DENARI, COPPE
+	}
+	
+	enum Value{
+		ASSO, DUE, TRE, QUATTRO, CINQUE, SEI, SETTE, FANTE, CAVALLO, RE
+	}
+	
+	/**
+	 * @return the suit of the card
+	 */
+	public Suit getSuit();
+	
+	/**
+	 * @return the value of the card
+	 */
+	public Value getValue();
+}
