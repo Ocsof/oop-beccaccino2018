fa9f5591b8a8073f2b5f50d7e56f48d1b58ec4f4
Davide Alpi <davide.alpi@studio.unibo.it>
2019-02-25 15:41:40 +0100

    Player class responsibility changed

--- src/model/entities/Player.java
+++ src/model/entities/Player.java
@@ -1,7 +1,5 @@
 package model.entities;
 
-import java.util.Optional;
-
 /**
  * A user playing the game
  */
@@ -15,17 +13,4 @@ public interface Player {
 	 * @return this player hand
 	 */
 	public Hand getHand();
-	
-	/**
-	 * @return an optional containing the AI controlling this player, or an empty
-	 *         optional if this player is meant to be user-controlled
-	 */
-	public Optional<AI> getAI();
-	
-	/**
-	 * @return true if this player is AI driven, false otherwise
-	 */
-	public default boolean isDrivenByAI() {
-		return this.getAI().isPresent();
-	};
 }
