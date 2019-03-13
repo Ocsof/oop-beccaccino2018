28366a90c09c34ae4fa609fe52d8f3fccda22e74
Davide Alpi <davide.alpi@studio.unibo.it>
2019-03-13 11:43:02 +0100

    Merge resolved

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -8,34 +8,22 @@ import model.entities.Play;
  */
 public interface GameView {
     /**
-<<<<<<< HEAD
-=======
      * Allows the players to make is play and returns it.
->>>>>>> 7eb43f054bc909f4f1888080192bc3065642fac6
+     * 
      * @return the play made by the user
      */
     Play getUserPlay();
 
     /**
-<<<<<<< HEAD
-=======
      * Allows the players to select a Briscola suit and returns it.
->>>>>>> 7eb43f054bc909f4f1888080192bc3065642fac6
+     * 
      * @return the suit selected by the user
      */
     Suit getSelectedBriscola();
 
     /**
-<<<<<<< HEAD
-     * @param the player whose play has already been made by the AI
-     * @param the play made by the AI
-=======
-     * Show the current round-play done.
->>>>>>> 7eb43f054bc909f4f1888080192bc3065642fac6
+     * Renders a play made by other entities.
      */
     void renderPlay();
 }
 
-/*
- * LA CLASSE CHE IMPLEMENTA QUESTA INTERFACCIA PRENDE DA COSTRUTTORE UN MATCH
- */
