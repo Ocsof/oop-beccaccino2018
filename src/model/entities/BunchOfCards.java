6ef4e8c571b1b525e8051ceee7d078c5622586c8
Davide Alpi <davide.alpi@studio.unibo.it>
2019-03-18 17:41:25 +0100

    Add method on utility class interface

--- src/model/entities/BunchOfCards.java
+++ src/model/entities/BunchOfCards.java
@@ -5,14 +5,15 @@ import java.util.List;
 import model.entities.ItalianCard.Suit;
 import model.entities.ItalianCard.Value;
 
-/** A simple card container with utility methods.
+/** 
+ * A simple card container with utility methods.
  */
-public interface CardSorter {
+public interface BunchOfCards {
     /**
      * Returns cards contained of specified suit.
      * 
      * @param suit - the suit you want to get cards of
-     * @return a list of cards of specified suit, ordered by value 
+     * @return a list of cards of specified suit, ordered by ascending value 
      */
     List<ItalianCard> getCardsOfSuit(Suit suit);
 
@@ -23,4 +24,11 @@ public interface CardSorter {
      * @return a list of cards of specified value 
      */
     List<ItalianCard> getCardsOfValue(Value value);
+
+    /**
+     * Compute the sum of points associated to each contained cards.
+     * 
+     * @return points contained cards are worth
+     */
+    int getPoints();
 }
