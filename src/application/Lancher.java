d92c6f41e1cddfad7bd0f32c530e4aaab2ff1d59
Riccardo Berti <riccardo.berti6@studio.unibo.it>
2019-04-09 18:54:48 +0200

    new Launcher class create

---
+++ src/application/Lancher.java
@@ -0,0 +1,19 @@
+package application;
+
+import menu.view.MainMenuView;
+import javafx.application.Application;
+
+public class Lancher {
+	
+	/**
+     * Main method.
+     *
+     * @param args
+     *            args
+     */
+	
+	public static void main(String[] args) {
+        Application.launch(MainMenuView.class, args);
+    }
+
+}
