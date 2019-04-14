ac37a9166ff2dde1d936572ab8f03ab819762647
Alessia Rocco <alessia.rocco@studio.unibo.it>
2019-04-14 16:46:02 +0200

    add hashCode and equals in TeamImpl

--- src/model/entities/TeamImpl.java
+++ src/model/entities/TeamImpl.java
@@ -3,9 +3,9 @@ package model.entities;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+
 /**
- * Alessia Rocco
- * Team implementation.
+ * Alessia Rocco Team implementation.
  */
 public class TeamImpl implements Team {
     private String teamName;
@@ -13,41 +13,104 @@ public class TeamImpl implements Team {
     private List<ItalianCard> cards;
     private int points;
 
-    public TeamImpl(final String teamName, List<Player> team) {
+    /**
+     * Class constructor.
+     * 
+     * @param teamName the name of the team
+     * @param team the team
+     */
+    public TeamImpl(final String teamName, final List<Player> team) {
         this.teamName = teamName;
         this.team = team;
         this.cards = new LinkedList<>();
         this.points = 0;
     }
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean addPlayer(final Player player) {
         return this.team.add(player);
     }
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public List<Player> getPlayers() {
         return Collections.unmodifiableList(this.team);
     }
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public void addWonCard(final ItalianCard card) {
         this.cards.add(card);
     }
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public List<ItalianCard> getWonCards() {
         return Collections.unmodifiableList(cards);
     }
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public void assignPoints(final int points) {
         this.points = this.points + points;
     }
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public int getPoints() {
         return this.points;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((cards == null) ? 0 : cards.hashCode());
+        result = prime * result + points;
+        result = prime * result + ((team == null) ? 0 : team.hashCode());
+        result = prime * result + ((teamName == null) ? 0 : teamName.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object team) {
+        if (this == team) {
+            return true;
+        }
+        if (team == null) {
+            return false;
+        }
+        if (getClass() != team.getClass()) {
+            return false;
+        }
+        TeamImpl other = (TeamImpl) team;
+        if (cards == null) {
+            if (other.cards != null) {
+                return false;
+            }
+        } else if (!cards.equals(other.cards)) {
+            return false;
+        }
+        if (points != other.points) {
+            return false;
+        }
+        if (!team.equals(other.team)) {
+            return false;
+        }
+        if (teamName == null) {
+            if (other.teamName != null) {
+                return false;
+            }
+        } else if (!teamName.equals(other.teamName)) {
+            return false;
+        }
+        return true;
+    }
 }
