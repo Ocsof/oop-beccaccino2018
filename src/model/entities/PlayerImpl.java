1f3a0b289988d137472b71a36de5796f9cf9c473
Alessia Rocco <alessia.rocco@studio.unibo.it>
2019-04-14 16:30:42 +0200

    add Hashcode and equals in PlayerImpl

--- src/model/entities/PlayerImpl.java
+++ src/model/entities/PlayerImpl.java
@@ -1,4 +1,5 @@
 package model.entities;
+
 /**
  * Alessia Rocco
  * Player Implementation.
@@ -6,20 +7,62 @@ package model.entities;
 public class PlayerImpl implements Player {
     private String name;
     private Hand hand;
-
+    /**
+     * Class constructor.
+     * @param name name of the player
+     * @param hand hand of the player
+     */
     public PlayerImpl(final String name, final Hand hand) {
-        super();
         this.name = name;
         this.hand = hand;
     }
-
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public String getName() {
         return this.name;
     }
-
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public Hand getHand() {
         return this.hand;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((hand == null) ? 0 : hand.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(final Object player) {
+        if (this == player) {
+            return true;
+        }
+        if (player == null) {
+            return false;
+        }
+        if (getClass() != player.getClass()) {
+            return false;
+        }
+        PlayerImpl other = (PlayerImpl) player;
+        if (hand == null) {
+            if (other.hand != null) {
+                return false;
+            }
+        } else if (!hand.equals(other.hand)) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        return true;
     }
 }
