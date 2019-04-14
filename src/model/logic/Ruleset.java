039f35ee7fd687970821194c67bfc9f0296c155b
Riccardo Berti <riccardo.berti6@studio.unibo.it>
2019-04-14 18:06:27 +0200

    Method createAI add

--- src/model/logic/Ruleset.java
+++ src/model/logic/Ruleset.java
@@ -1,17 +1,35 @@
 package model.logic;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
-import model.entities.AI;
 import model.entities.Player;
 import view.GameView;
 
+/**
+ * The ruleset which creates all the entities and objects for a game.
+ */
 public interface Ruleset {
+    /**
+     * This method creates a new Game object.
+     * @param players - the list of all the players for the current game.
+     * @return The game created.
+     */
     Game newGame(List<Player> players);
 
+    /**
+     * This method creates a new Game object.
+     * @param game - the game for which the view has to be created.
+     * @return The game view created.
+     */
     GameView newGameView(Game game);
-    
+    /**
+     * This method creates a new Game object.
+     * @param name - the name of the player.
+     * @return The player created.
+     */
     Player newPlayer(String name);
+    /**
+     * This method creates a new Game object.
+     * @return The AI created.
+     */
+    AIImpl newAI();
 }
