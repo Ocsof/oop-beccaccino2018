02061608890920944b347cdfe14236a0a3d6d961
Riccardo Berti <riccardo.berti6@studio.unibo.it>
2019-04-14 18:08:17 +0200

    Checkstyle warning fix

--- src/menu/control/PreGameMenuControlImpl.java
+++ src/menu/control/PreGameMenuControlImpl.java
@@ -13,48 +13,47 @@ import javafx.scene.control.ComboBox;
 import menu.view.MainMenuView;
 import model.entities.AI;
 import model.entities.Player;
-import model.entities.PlayerImpl;
 import model.logic.Game;
 import model.logic.Ruleset;
 import util.UtilityClass;
 import view.GameView;
-import view.GameViewImpl;
-
+/**
+ * This is an Implementation of the Interface PreGameMenuControl.
+ */
 public class PreGameMenuControlImpl implements PreGameMenuControl {
-	
-	@FXML
-	private ComboBox<String> boxPlayer1;
-	@FXML
-	private ComboBox<String> boxPlayer2;
-	@FXML
-	private ComboBox<String> boxPlayer3;
-	@FXML
-	private ComboBox<String> boxPlayer4;
+    @FXML
+    private ComboBox<String> boxPlayer1;
+    @FXML
+    private ComboBox<String> boxPlayer2;
+    @FXML
+    private ComboBox<String> boxPlayer3;
+    @FXML
+    private ComboBox<String> boxPlayer4;
 
-	@Override
-	public void backClicked(ActionEvent event) {
-		
-		MainMenuView.mainMenuSetup(UtilityClass.returnStageOf(event));
-
-	}
-
-	@Override
-	public void startClicked(ActionEvent event) {
-		//chiedi ad alessia per vedere cosa passare e cosa no
-		Ruleset ruleset = null;
-		List<Player> playerList = new ArrayList<Player>();
-		playerList.add(ruleset.newPlayer("Player1"));
-		playerList.add(ruleset.newPlayer("Player2"));
-		playerList.add(ruleset.newPlayer("Player3"));
-		playerList.add(ruleset.newPlayer("Player4"));
-		Game currentGame = ruleset.newGame(playerList);
-		GameView currentGameView = ruleset.newGameView(currentGame);
-		Map<Player, Optional<AI>> playerMap = new HashMap<Player, Optional<AI>>();
-		playerMap.put(playerList.get(0),null);
-		playerMap.put(playerList.get(1),null);
-		playerMap.put(playerList.get(2),null);
-		playerMap.put(playerList.get(3),null);
-		GameController currentGameController = new GameController(playerMap,currentGame,currentGameView);
-;	}
+    /**
+     * {@inheritDoc}
+     */
+    public void backClicked(final ActionEvent event) {
+        MainMenuView.mainMenuSetup(UtilityClass.returnStageOf(event));
+        }
+    /**
+     * {@inheritDoc}
+     */
+    public void startClicked(final ActionEvent event) {
+        Ruleset ruleset = null;
+        List<Player> playerList = new ArrayList<Player>();
+        playerList.add(ruleset.newPlayer("Player1"));
+        playerList.add(ruleset.newPlayer("Player2"));
+        playerList.add(ruleset.newPlayer("Player3"));
+        playerList.add(ruleset.newPlayer("Player4"));
+        Game currentGame = ruleset.newGame(playerList);
+        GameView currentGameView = ruleset.newGameView(currentGame);
+        Map<Player, Optional<AI>> playerMap = new HashMap<Player, Optional<AI>>();
+        playerMap.put(playerList.get(0), null);
+        playerMap.put(playerList.get(1), ruleset.newAI("easy"));
+        playerMap.put(playerList.get(2), ruleset.newAI("easy"));
+        playerMap.put(playerList.get(3), ruleset.newAI("easy"));
+        GameController currentGameController = new GameController(playerMap, currentGame, currentGameView);
+    }
 
 }
