301a6f4c9017214a221bb352dc4732759c3a9584
Alessia Rocco <alessia.rocco@studio.unibo.it>
2019-04-16 20:11:08 +0200

    bug fix in GameViewImpl and UserPlay

--- src/view/GameViewImpl.java
+++ src/view/GameViewImpl.java
@@ -18,29 +18,34 @@ import model.entities.ItalianCard.Suit;
 import model.entities.Play;
 import model.entities.PlayImpl;
 import model.logic.Game;
+
 /**
- * Alessia Rocco
- * GameView Implementation.
+ * Alessia Rocco GameView Implementation.
  */
 public class GameViewImpl implements GameView {
     private Game game;
     private final int size = 80;
-    private final BackgroundImage tavolo = new BackgroundImage(new Image("file:tavolo.jpg"),
-            BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+    private final BackgroundImage tavolo = new BackgroundImage(new Image("file:tavolo.jpg"), BackgroundRepeat.NO_REPEAT,
+            BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
     private List<Node> boxes = new LinkedList<>();
     private List<ItalianCard> tableCards = new LinkedList<>();
     private Stage primaryStage;
     private Map<Node, ItalianCard> map = new HashMap<>();
     private Map<ItalianCard, Node> map2 = new HashMap<>();
+    private boolean waitCondition;
+
     /**
      * Class constructor.
+     * 
      * @param game model.Match
      */
     public GameViewImpl(final Game game) {
         this.game = game;
     }
+
     /**
      * The method to set up the Stage of the initial game.
+     * 
      * @param primaryStage the Stage.
      */
     public void initialGameSetUp(final Stage primaryStage) {
@@ -62,8 +67,9 @@ public class GameViewImpl implements GameView {
 
         externalPane.setBackground(backGround);
 
-        /*qui da risistemare i numeri secchi con delle proporzioni e togliere magic number
-         * sostituendoli con una variabile.
+        /*
+         * qui da risistemare i numeri secchi con delle proporzioni e togliere
+         * magic number sostituendoli con una variabile.
          */
         roottop.setPadding(new Insets(10, 10, 10, this.size * 5));
         rootleft.setPadding(new Insets(this.size, 0, 0, 0));
@@ -83,51 +89,62 @@ public class GameViewImpl implements GameView {
         this.primaryStage.setWidth(Toolkit.getDefaultToolkit().getScreenSize().getWidth() * 0.8);
         this.primaryStage.show();
     }
+
     /**
      * A method in order to set Players' Hand in the table.
-     * @param boxes: list of the five region of boxes (where the cards are added)
+     * 
+     * @param boxes: list of the five region of boxes (where the cards are
+     * added)
      */
     private void setPlayersHand(final List<Node> boxes) {
-     /*
-      *setting the 4 ItalianCardsDeck: clockwise, starting from the bottom.
-      */
-     for (int i = 0; i < 4; i++) {
-         for (ItalianCard card: game.getPlayers().get(i).getHand().getCards()) {
-             ItalianCardViewFactory c = new ItalianCardView(card);
-             this.map.put(c.getCardRepresentation(card), card);
-             ((Pane) boxes.get(i)).getChildren().add(c.getCardRepresentation(card));
-         }
-     }
+        /*
+         * setting the 4 ItalianCardsDeck: clockwise, starting from the bottom.
+         */
+        for (int i = 0; i < 4; i++) {
+            for (ItalianCard card : game.getPlayers().get(i).getHand().getCards()) {
+                ItalianCardViewFactory c = new ItalianCardView(card);
+                this.map.put(c.getCardRepresentation(card), card);
+                ((Pane) boxes.get(i)).getChildren().add(c.getCardRepresentation(card));
+            }
+        }
     }
+
     /**
-     * A method in order to set cards played during a Round in the centre of the table.
+     * A method in order to set cards played during a Round in the center of the
+     * table.
+     * 
      * @param boxes list of the five region of boxes (where the cards are added)
      */
     private void setTableCards(final List<Node> boxes) {
-        for (ItalianCard card: this.tableCards) {
+        for (ItalianCard card : this.tableCards) {
             ItalianCardViewFactory c = new ItalianCardView(card);
             ((Pane) boxes.get(this.boxes.size() - 1)).getChildren().add(c.getCardRepresentation(card));
         }
     }
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public Play getUserPlay() {
-        /*aggancio action listener: attivo le carte giocabili (vedi metodo in GameViewObserver)
-        *condwait su Playscelta
-        *quando verremo "svegliati" abbiamo la play, controllo che la currentPlay 
-        *sia piena e la ritorno.
-        */
-        //creo il nuovo thread che si occupa della giocata
-        UserPlay userPlay = new UserPlay(this.game, this.boxes, this.tableCards, this.map2, this.map, this.primaryStage);
+        /*
+         * aggancio action listener: attivo le carte giocabili (vedi metodo in
+         * GameViewObserver) condwait su Playscelta quando verremo "svegliati"
+         * abbiamo la play, controllo che la currentPlay sia piena e la ritorno.
+         */
+        // creo il nuovo thread che si occupa della giocata
+        UserPlay userPlay = new UserPlay(this.game, this.boxes, this.tableCards, this.map2, this.map,
+                this.primaryStage, this.waitCondition);
         userPlay.start();
-        //WAIT DEL MAIN THREAD
-        try {
-            wait();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        // WAIT DEL MAIN THREAD
+        while (!this.waitCondition) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        //qui in teoria lascio la parola alla classe UserPlay che esegue tutto
-        //SVEGLIO IL MAINTHREAD
+        // qui in teoria lascio la parola alla classe UserPlay che esegue tutto
+        // SVEGLIO IL MAINTHREAD
         PlayImpl play = userPlay.getPlay();
         this.tableCards.add(play.getCard());
         Node lastPlayNode = this.map2.get(play.getCard());
@@ -138,22 +155,28 @@ public class GameViewImpl implements GameView {
         return play;
     }
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public Suit getSelectedBriscola() {
-        /*fare in modo di selezionare una briscola in modo view, 
-         * settare un campo briscola e poi ritornarlo.
-         * come getUserPlay, una nuova finestra con 4 bottoni da selezionare, 
-         * non si deve oscurare la roba sotto (una DIALOG).
+        /*
+         * fare in modo di selezionare una briscola in modo view, settare un
+         * campo briscola e poi ritornarlo. come getUserPlay, una nuova finestra
+         * con 4 bottoni da selezionare, non si deve oscurare la roba sotto (una
+         * DIALOG).
          */
         BriscolaView briscola = new BriscolaView(this.primaryStage);
         return briscola.getSuit().get();
     }
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public void renderPlay() {
-        /*visualizza la carta giocata, la ruota e la toglie dalla mano del giocatore
-         * poi se è l'ultima giocata toglie le carte e le assegna a chi ha vinto il turno.
-         * altrimenti si passa al prossimo.
+        /*
+         * visualizza la carta giocata, la ruota e la toglie dalla mano del
+         * giocatore poi se è l'ultima giocata toglie le carte e le assegna a
+         * chi ha vinto il turno. altrimenti si passa al prossimo.
          */
         List<Play> p = this.game.getCurrentRound().getPlays();
         Play lastPlay = p.get(p.size() - 1);
--- src/view/UserPlay.java
+++ src/view/UserPlay.java
@@ -10,9 +10,9 @@ import javafx.stage.Stage;
 import model.entities.ItalianCard;
 import model.entities.PlayImpl;
 import model.logic.Game;
+
 /**
- * Alessia Rocco
- * Class UserPlay.
+ * Alessia Rocco Class UserPlay.
  */
 public class UserPlay extends Thread {
     private Game game;
@@ -25,52 +25,65 @@ public class UserPlay extends Thread {
     private PlayImpl play;
     private Stage primaryStage;
     private MessageView mess;
+    private boolean waitCondition;
+
     /**
      * Class constructor.
-     * @param game the game 
+     * 
+     * @param game the game
      * @param boxes the boxes
      * @param tableCards the tableCards
-     * @param map 
-     * @param map2 
+     * @param map first map
+     * @param map2 second map
      * @param primaryStage the initial stage
+     * @param waitCondition the condition
      */
-    public UserPlay(final Game game, final List<Node> boxes, final List<ItalianCard> tableCards, 
-            final Map<ItalianCard, Node> map2, final Map<Node, ItalianCard> map, final Stage primaryStage) {
+    public UserPlay(final Game game, final List<Node> boxes, final List<ItalianCard> tableCards,
+            final Map<ItalianCard, Node> map2, final Map<Node, ItalianCard> map, final Stage primaryStage,
+            final boolean waitCondition) {
         this.game = game;
-        //la box contenente le carte dello user è sempre in posizione 0 della lista boxes.
+        // la box contenente le carte dello user è sempre in posizione 0 della
+        // lista boxes.
         this.box = (HBox) boxes.get(0);
         this.tableCards = tableCards;
         this.map = map;
         this.map2 = map2;
         this.game.getPlayers().get(0).getHand().getCards();
         this.primaryStage = primaryStage;
+        this.waitCondition = waitCondition;
+
         this.enableCards();
     }
-    //abilito le carte giocabili
+
+    // abilito le carte giocabili
     /**
-     * method that, with the Hand Cards, select the only ones that are able to be
-     * played.
+     * method that, with the Hand Cards, select the only ones that are able to
+     * be played.
      */
     public void enableCards() {
-        for (Node card: this.box.getChildren()) {
+        for (Node card : this.box.getChildren()) {
             if (this.game.getCurrentRound().getPlayableCards().contains(this.map.get(card))) {
                 card.setOnMouseClicked(s -> {
                     this.cardPlayed = this.map.get(card);
-                    /* add the choice of the message associated to the card (in order to the card there
-                     * are available messages.
+                    /*
+                     * add the choice of the message associated to the card (in
+                     * order to the card there are available messages.
                      */
                     mess = new MessageView(this.primaryStage, this.game, this.cardPlayed);
                     this.game.getCurrentPlayer().getHand().getCards().remove(this.cardPlayed);
                     this.tableCards.add(this.cardPlayed);
                     this.message = this.mess.getMessage();
-                    //sveglio il main
+                    // sveglio il main
+                    this.waitCondition = true;
                     notify();
                 });
             }
         }
     }
+
     /**
      * Play getter.
+     * 
      * @return the play just played
      */
     public PlayImpl getPlay() {
