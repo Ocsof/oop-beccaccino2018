dc42ddc7752e3479fde01b772a9af02d165dd168
Riccardo Berti <riccardo.berti6@studio.unibo.it>
2019-04-16 15:12:55 +0200

    Javadoc add

--- src/application/Launcher.java
+++ src/application/Launcher.java
@@ -2,18 +2,21 @@ package application;
 
 import menu.view.MainMenuView;
 import javafx.application.Application;
-
-public class Lancher {
-	
-	/**
+/**
+ * The launcher containing the main method and the call to Application.launch.
+ */
+public final class Launcher {
+    /**
      * Main method.
      *
      * @param args
      *            args
      */
-	
-	public static void main(String[] args) {
+    public static void main(final String[] args) {
         Application.launch(MainMenuView.class, args);
+    }
+
+    private Launcher() {
     }
 
 }
