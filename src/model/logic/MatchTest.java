2985a2961905dfdb1a7e2f8c37ddfa316f5a1ee4
Davide Alpi <davide.alpi@studio.unibo.it>
2019-04-17 12:40:29 +0200

    Create class for testing game without view

--- src/model/logic/MatchTest.java
+++ src/model/logic/MatchTest.java
@@ -1,19 +1,49 @@
 package model.logic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import static org.junit.Assert.fail;
-
-import org.junit.Test;
+import model.artificialIntelligence.AI;
+import model.entities.Play;
+import model.entities.Player;
 
 /**
  * JUnit test for class implementing Game.
  */
 public class MatchTest {
-    private Match match = null;
+    public static void main(final String[] args) {
+        final Ruleset ruleset = new RulesetImpl();
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
+        List<Player> playerList = new ArrayList<Player>();
+        playerList.add(ruleset.newPlayer("Player1"));
+        playerList.add(ruleset.newPlayer("Player2"));
+        playerList.add(ruleset.newPlayer("Player3"));
+        playerList.add(ruleset.newPlayer("Player4"));
+        Game game = ruleset.newGame(playerList);
+        Map<Player, AI> playingEntities = new HashMap<>();
+        playingEntities.put(playerList.get(0), ruleset.newAI(playerList.get(0)).get());
+        playingEntities.put(playerList.get(1), ruleset.newAI(playerList.get(1)).get());
+        playingEntities.put(playerList.get(2), ruleset.newAI(playerList.get(2)).get());
+        playingEntities.put(playerList.get(3), ruleset.newAI(playerList.get(3)).get());
+        while (!game.isOver()) {
+            final Player currentPlayer = game.getCurrentPlayer();
+            final AI ai = playingEntities.get(currentPlayer);
+            if (!game.getBriscola().isPresent()) {
+                game.setBriscola(ai.selectBriscola());
+                for (AI intelligence : playingEntities.values()) {
+                    intelligence.setBriscola(game.getBriscola().get());
+                }
+            }
+            final Play play = ai.makePlay(game.getCurrentRound());
+            game.makeTurn(play);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
-
 }
