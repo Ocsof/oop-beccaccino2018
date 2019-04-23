cf71bd8f3bd792ba40a3825101e29921ae093f80
Riccardo Berti <riccardo.berti6@studio.unibo.it>
2019-04-23 16:00:52 +0200

    SceneLauncher javadoc add

--- src/util/SceneLauncher.java
+++ src/util/SceneLauncher.java
@@ -3,16 +3,18 @@ package util;
 import javafx.scene.Parent;
 
 /**
- * This is the Interface for an FXML file loader
+ * This is the Interface for an FXML file loader.
  */
 public interface SceneLauncher {
     /**
-     * This method loads the FXML file previously set 
+     * This method loads the FXML file previously set.
      * @return Parent 
      */
     Parent launchScene();
     /**
-     * This is the Interface for an FXML file loader
+     * This method sets the name of the Scene to be loaded.
+     * Created specifically in case a Launcher has ti be used more than once to load a Scene.
+     * @param newStringName - the name of the scene to be loaded.
      */
     void setSceneName(String newStringName);
 
