604f8f4e5506c8eacdf40a1657131b7743d83410
Riccardo Berti <riccardo.berti6@studio.unibo.it>
2019-04-25 11:10:39 +0200

    Profile creation methods create and javadoc add

--- src/menu/control/ProfileMenuControl.java
+++ src/menu/control/ProfileMenuControl.java
@@ -1,13 +1,18 @@
 package menu.control;
 
 import javafx.event.ActionEvent;
-
+/**
+ * This Interface contains all controller methods regarding the Profile Menu Scene.
+ */
 public interface ProfileMenuControl {
-	
-	void confirmationClicked();
-	
-	void backClicked(ActionEvent event);
-	
-	void createClicked();
-
+    /**
+     * Controller method for the pressing of the Back button on the Profile Menu.
+     * @param event - the event triggered by the pressing of the Back Button.
+     */
+    void backClicked(ActionEvent event);
+    /**
+     * Controller method for the pressing of the Create Profile button on the Profile Menu.
+     * @param event - the event triggered by the pressing of the Create Profile Button.
+     */
+    void createClicked(ActionEvent event);
 }
--- src/menu/control/ProfileMenuControlImpl.java
+++ src/menu/control/ProfileMenuControlImpl.java
@@ -1,28 +1,48 @@
 package menu.control;
 
 import javafx.event.ActionEvent;
-import menu.view.MainMenuView;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
+
+import menu.view.MenuView;
 import util.UtilityClass;
+/**
+ * This is an implementation of the Interface ProfileMenuControl.
+ */
+public class ProfileMenuControlImpl implements ProfileMenuControl {
+    @FXML
+    private TextField nameField;
 
-public class ProfileMenuControlImpl implements ProfileMenuControl{
-
-	@Override
-	public void confirmationClicked() {
-		
-	}
-
-	@Override
-	public void backClicked(ActionEvent event) {
-		
-		MainMenuView.mainMenuSetup(UtilityClass.returnStageOf(event));
-		
-	}
-
-	@Override
-	public void createClicked() {
-		
-	}
-	
-	
-
+    /**
+     * {@inheritDoc}
+     */
+    public void backClicked(final ActionEvent event) {
+        MenuView.menuSetup(UtilityClass.returnStageOf(event), "MainMenuScene.fxml");
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public void createClicked(final ActionEvent event) {
+        if (nameField.getText().isEmpty()) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Empty Profile Name");
+            alert.setContentText("You can't create a profile with an empty name.");
+            alert.showAndWait();
+        } else if (!UtilityClass.createProfile(nameField.getText())) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Invalid Name");
+            alert.setContentText("A profile with this name already exists or the name chosen contains invalid characters.");
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Profile Created");
+            alert.setHeaderText(null);
+            alert.setContentText("The profile was created successfully.");
+            alert.showAndWait();
+        }
+    }
 }
