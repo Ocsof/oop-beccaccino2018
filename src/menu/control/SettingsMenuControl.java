5e12b239074e3ab90fcd03b916a4ca0810a95be3
Riccardo Berti <riccardo.berti6@studio.unibo.it>
2019-04-25 11:23:26 +0200

    SettingsMenuControl modify

--- src/menu/control/SettingsMenuControl.java
+++ src/menu/control/SettingsMenuControl.java
@@ -3,7 +3,5 @@ package menu.control;
 import javafx.event.ActionEvent;
 
 public interface SettingsMenuControl {
-
-	public void backClicked(ActionEvent event);
-	
+    public void backClicked(ActionEvent event);
 }
--- src/menu/control/SettingsMenuControlImpl.java
+++ src/menu/control/SettingsMenuControlImpl.java
@@ -1,17 +1,12 @@
 package menu.control;
 
 import javafx.event.ActionEvent;
-import menu.view.MainMenuView;
+import menu.view.MenuView;
 import util.UtilityClass;
 
 public class SettingsMenuControlImpl implements SettingsMenuControl {
-
-	@Override
-	public void backClicked(ActionEvent event) {
-		
-		MainMenuView.mainMenuSetup(UtilityClass.returnStageOf(event));
-		
-	}
-	
-
+    @Override
+    public void backClicked(final ActionEvent event) {
+        MenuView.menuSetup(UtilityClass.returnStageOf(event), "MainMenuScene.fxml");
+    }
 }
