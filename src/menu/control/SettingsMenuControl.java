2dc7818616e16932aea0b03b6e34f81ab6dedcec
Riccardo Berti <riccardo.berti6@studio.unibo.it>
2019-04-28 13:30:40 +0200

    initialize method and injectable fields create in settings menu

--- src/menu/control/SettingsMenuControl.java
+++ src/menu/control/SettingsMenuControl.java
@@ -6,8 +6,12 @@ import javafx.event.ActionEvent;
  */
 public interface SettingsMenuControl {
     /**
-     * This Interface contains all controller methods regarding the Settings Menu Scene.
+     * Controller method for the pressing of the Back button on the Settings Menu.
      * @param event - the event triggered by the pressing of the Back Button.
      */
     void backClicked(ActionEvent event);
+    /**
+     * This is the method called by the FXML file after the injection of all FXML items.
+     */
+    void initialize();
 }
--- src/menu/control/SettingsMenuControlImpl.java
+++ src/menu/control/SettingsMenuControlImpl.java
@@ -1,16 +1,32 @@
 package menu.control;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import menu.view.MenuView;
 import util.UtilityClass;
 /**
  * This is an implementation of the Interface SettingsMenuControl.
  */
 public class SettingsMenuControlImpl implements SettingsMenuControl {
+    @FXML
+    private CheckBox musicBox;
+    @FXML
+    private CheckBox voiceBox;
+    @FXML
+    private CheckBox soundFXBox;
+    @FXML
+    private CheckBox criccaBox;
     /**
      * {@inheritDoc}
      */
     public void backClicked(final ActionEvent event) {
         MenuView.menuSetup(UtilityClass.returnStageOf(event), "MainMenuScene.fxml");
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public void initialize() {
+        
     }
 }
