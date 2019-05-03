3fa8023d1bbebdc4df66d02afbe9df427bd44eff
Riccardo Berti <riccardo.berti6@studio.unibo.it>
2019-05-03 18:16:38 +0200

    music voices and sfx remove

--- src/menu/control/SettingsMenuControlImpl.java
+++ src/menu/control/SettingsMenuControlImpl.java
@@ -20,12 +20,6 @@ public class SettingsMenuControlImpl implements SettingsMenuControl {
     @FXML
     private BorderPane borderPane;
     @FXML
-    private CheckBox musicBox;
-    @FXML
-    private CheckBox voiceBox;
-    @FXML
-    private CheckBox soundFXBox;
-    @FXML
     private CheckBox criccaBox;
     /**
      * {@inheritDoc}
@@ -38,12 +32,6 @@ public class SettingsMenuControlImpl implements SettingsMenuControl {
                                                                     + "res" + System.getProperty("file.separator") + "settings.txt"));
         String line = reader.readLine();
         criccaBox.setSelected(line.equals("points_for_cricca: TRUE"));
-        line = reader.readLine();
-        musicBox.setSelected(line.equals("music: TRUE"));
-        line = reader.readLine();
-        voiceBox.setSelected(line.equals("voices: TRUE"));
-        line = reader.readLine();
-        soundFXBox.setSelected(line.equals("sfx: TRUE"));
         reader.close();
         } catch (IOException e) {
             System.out.println("Error in reading Settings file.\nShutting down...");
@@ -63,21 +51,6 @@ public class SettingsMenuControlImpl implements SettingsMenuControl {
                 writer.write("points_for_cricca: TRUE\n");
             } else {
                 writer.write("points_for_cricca: FALSE\n");
-            }
-            if (musicBox.isSelected()) {
-                writer.write("music: TRUE\n");
-            } else {
-                writer.write("music: FALSE\n");
-            }
-            if (voiceBox.isSelected()) {
-                writer.write("voices: TRUE\n");
-            } else {
-                writer.write("voices: FALSE\n");
-            }
-            if (soundFXBox.isSelected()) {
-                writer.write("sfx: TRUE\n");
-            } else {
-                writer.write("sfx: FALSE\n");
             }
             writer.close();
         } catch (FileNotFoundException e) {
